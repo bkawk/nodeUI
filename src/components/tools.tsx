6ac70d20d0e8bce5aed96f60c111e1576941e02c
bkawk <email.will.in.china@gmail.com>
2020-02-25 18:27:35 +0800

    align icons

--- src/components/tools.tsx
+++ src/components/tools.tsx
@@ -1,8 +1,14 @@
 import React, { useContext, useState } from 'react';
 import { Dispatch, TOGGLE_SELECTOR, TOGGLE_SNAP } from '../globalState';
+import bottomImage from '../images/bottom.svg';
+import centerImage from '../images/center.svg';
 import fullScreenImage from '../images/full-screen.svg';
+import leftImage from '../images/left.svg';
+import middleImage from '../images/middle.svg';
 import pointerImage from '../images/pointer.svg';
+import rightImage from '../images/right.svg';
 import snapImage from '../images/snap.svg';
+import topImage from '../images/top.svg';
 
 const Tools: React.FC = () => {
   const { dispatch } = useContext(Dispatch);
@@ -35,6 +41,30 @@ const Tools: React.FC = () => {
         <div className={`${selector ? 'tools--box-on' : 'tools--box'}`} onClick={toggleSelector}>
           <img src={pointerImage} alt='pooiinter' />
         </div>
+
+        <div className={`${selector ? 'tools--box-on' : 'tools--box'}`} onClick={toggleSelector}>
+          <img src={leftImage} alt='pooiinter' />
+        </div>
+
+        <div className={`${selector ? 'tools--box-on' : 'tools--box'}`} onClick={toggleSelector}>
+          <img src={centerImage} alt='pooiinter' />
+        </div>
+
+        <div className={`${selector ? 'tools--box-on' : 'tools--box'}`} onClick={toggleSelector}>
+          <img src={rightImage} alt='pooiinter' />
+        </div>
+
+        <div className={`${selector ? 'tools--box-on' : 'tools--box'}`} onClick={toggleSelector}>
+          <img src={topImage} alt='pooiinter' />
+        </div>
+
+        <div className={`${selector ? 'tools--box-on' : 'tools--box'}`} onClick={toggleSelector}>
+          <img src={middleImage} alt='pooiinter' />
+        </div>
+        <div className={`${selector ? 'tools--box-on' : 'tools--box'}`} onClick={toggleSelector}>
+          <img src={bottomImage} alt='pooiinter' />
+        </div>
+
         <div className={`${snap ? 'tools--box-on' : 'tools--box'}`} onClick={toggleSnap}>
           <img src={snapImage} alt='snap' />
         </div>
